eea39a77f73c0eb18721fcd55ee60a68aec00e12
fareldev <ffarelscho@gmail.com>
2025-10-22 10:10:31 +0700

    Add ModifyWhileLoop Study Kasus

---
+++ ModifySiakadWhile08.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ModifySiakadWhile08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jml,  nilai, i = 0;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jml = sc.nextInt();
+
+        while (i < jml) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai = sc.nextInt();
+            
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid!. Masukkan nilai antara 0-100.");
+                continue; // Tolong Masukkan nilai yang valid
+            }
+
+            if (nilai >= 80 && nilai <= 100) {
+                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah A");
+                // Modifikasi: Menambahkan Pesan Baru
+                System.out.println("Bagus, pertahankan nilainya");
+            } else if (nilai > 73 && nilai <= 80) {
+                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah B+");
+            } else if (nilai > 65 && nilai <= 73) {
+                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah B");
+            } else if (nilai > 60 && nilai <= 65) {
+                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah C+");
+            } else if (nilai > 50 && nilai <= 60) {
+                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah C");
+            } else if (nilai > 39 && nilai <= 50) {
+                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah D");
+            } else {
+                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah E");
+            }
+            i++;
+        }
+    }
+}
